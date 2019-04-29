6636d5760a7ab62dd8c36eef705ffb8896cf9b86
briefausde <sirkiza@gmail.com>
2019-04-29 16:59:08 +0300

    fx forms.py __init__ duplicate

--- engine/forms.py
+++ engine/forms.py
@@ -6,28 +6,24 @@ from .models import User
 # TODO: __init__ duplicate
 
 
-class LoginForm(AuthenticationForm):
+class BootstrapMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs = {
                 'class': 'form-control'
             }
+
+
+class LoginForm(BootstrapMixin, AuthenticationForm):
 
     class Meta:
         model = User
         fields = ('username', 'password')
 
 
-class RegisterForm(UserCreationForm):
+class RegisterForm(BootstrapMixin, UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs = {
-                'class': 'form-control'
-            }
 
     class Meta:
         model = User
